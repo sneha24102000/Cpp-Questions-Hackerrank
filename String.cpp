cc10f84e31fa6faf2bb26b76f841a460c2c8a5a1
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-02-21 21:07:19 +0530

    Add files via upload

---
+++ String.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a,b;
+    cin>>a>>b;
+    
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<a+b<<endl;
+    
+    char t1=a[0];
+    char t2=b[0];
+    
+    a[0]=t2;
+    b[0]=t1;
+    
+    cout<<a<<" "<<b;
+    return 0;
+    
+    
+    
+  
+    return 0;
+}
+
+
+
+
+
+
+
+
