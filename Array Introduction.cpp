6dbbd9d9325ba3265bf03c43912d8914d975ada4
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-02-21 20:29:01 +0530

    Add files via upload

---
+++ Array Introduction.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int size;
+cin>> size;
+int arr[size]; 
+for (int i=0;i<size; i++){
+    cin>>arr[i];
+}
+for (int i=size-1;i>=0; i--){
+    cout<<arr[i]<<" ";
+}
+    return 0;
+}
+
+
+
+
+
