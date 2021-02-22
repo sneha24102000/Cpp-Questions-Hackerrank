246fae4af1bc78e30d149f1a5299bdadf48136ac
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-02-22 23:53:15 +0530

    Add files via upload

---
+++ Preprocessor Solution.cpp
@@ -0,0 +1,8 @@
+/* Enter your macros here */
+#define toStr(str) #str
+#define foreach(v, i) for(int i = 0; i< v.size(); i++)
+#define io(vec) cin >> vec
+#define FUNCTION(a, b)
+#define minimum(a, b) a = (a < b)  ? a : b
+#define maximum(a, b) a = (b < a)  ? a:b
+#define INF 0
